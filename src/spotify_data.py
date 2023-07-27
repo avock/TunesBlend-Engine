f632b19687b9685fa065ddfa0515fd9247fc73b3
Chun Khai <>
2023-07-27 14:25:10 +0800

    wip

--- src/spotify_data.py
+++ src/spotify_data.py
@@ -3,6 +3,15 @@ import datetime, pprint
 from src.data_processing import *
 from src.misc import *
 
+
+
+"""
+Spotify Constants
+"""
+genre_list = ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
+
+
+
 """
 Function to retrieve audio_features of a track
 
@@ -148,12 +157,13 @@ def get_track_genre(playlist, sp):
     
     for i in range(0, len(track_uris), chunk_size):
         chunk = track_uris[i:i + chunk_size]
+        print(f"playlist {i}:{i+chunk_size}")
         artist_genre_chunk = get_artist_genre(chunk, sp)
         album_genre_chunk = get_album_genre(chunk, sp)
         
         track_genre_chunk = arr_cleanup(arr_combine(artist_genre_chunk, album_genre_chunk))
-        
         pid = playlist['pid']
+        print(pid)
         for genre in track_genre_chunk:
             id = f"{pid}_{track_id}"
             # genre['id'] = id
