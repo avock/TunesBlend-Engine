6509597fcc6c3f39c2dce341eecabec2f7e5ea67
Chun Khai <chunkhai.k@gmail.com>
2023-08-14 11:19:01 +0800

    Jupyter: Add Visualization to cluster size

--- main.py
+++ main.py
@@ -41,10 +41,13 @@ def main():
     # for genre in global_genres[:1]:
     #     pprint(get_spotify_search(sp, 50, genre=genre))
     #     print(genre)
-    track_uri = 'spotify:track:3EaJDYHA0KnX88JvDhL9oa'
-    search_track_name = 'Rast Makamı - Turkish Music Therapy'
+    track_uri = 'spotify:track:1neFq9Jrg8jprpQaNUlHRb'
+    search_track_name = 'The Nights'
     
-    print(get_audio_features(sp, (get_spotify_search(sp, track=search_track_name))['tracks'][0]['track_uri']))
+    # print(get_audio_features(sp, (get_spotify_search(sp, track=search_track_name))['tracks'][0]['track_uri']))
+    # print(get_audio_features(sp, track_uri))
+    pprint(get_track_details(sp, track_uri))
+
         
 if __name__ == "__main__":
     main()
